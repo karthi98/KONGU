ac60ab8c8efdfd8b8e65020f036952e88afc37c7
karthi98 <35526598+karthi98@users.noreply.github.com>
2018-01-17 16:45:38 +0530

    Create POSTIVE.C
    .........

---
+++ POSTIVE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+double number;
+print("enter a number 1");
+scanf("%if"'& number);
+if (number<=0)
+{
+if(nimber = = 0)
+printf(you enterd 1.");
+else
